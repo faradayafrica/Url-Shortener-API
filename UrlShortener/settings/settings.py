d91aacf0626f6fefe68e2c9bcf51fa30d3e5615d
devgenix <emmanueln644@gmail.com>
2021-12-31 03:15:24 +0100

    updated allowed hosts

--- UrlShortener/settings/settings.py
+++ UrlShortener/settings/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "False"
 
-ALLOWED_HOSTS = ['shortened-lu4y8.ondigitalocean.app', 'frda.me']
+ALLOWED_HOSTS = ['frda.ondigitalocean.app', 'frda.me', 'api.frda.me']
 
 
 # Application definition

bfee402b680805f74c7b9bfc505726abdfe9b2c1
devgenix <emmanueln644@gmail.com>
2021-12-31 10:27:42 +0100

    Update: updated settings, added env variable for allowed hosts

--- UrlShortener/settings/settings.py
+++ UrlShortener/settings/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "False"
 
-ALLOWED_HOSTS = ['frda.ondigitalocean.app', 'frda.me', 'api.frda.me']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition

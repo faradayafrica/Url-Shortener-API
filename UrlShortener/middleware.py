0ec482290e590a106c95d4c23c5a0097f7ff8167
devgenix <emmanueln644@gmail.com>
2022-01-01 22:37:36 +0100

    Update: modified CORS and added frontend site to allowed hosts

--- UrlShortener/middleware.py
+++ UrlShortener/middleware.py
@@ -1,17 +1,17 @@
+from django import http
+
 class CustomCorsMiddleware:
+
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
         response = self.get_response(request)
+        if (request.method == "OPTIONS"  and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
+            response = http.HttpResponse()
+            response["Content-Length"] = "0"
+            response["Access-Control-Max-Age"] = 86400
         response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Headers"] = "*"
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
+        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
         return response

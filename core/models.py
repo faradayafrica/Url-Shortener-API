f1f29f6c0e964a1a06204881ccaa63c6c7d816d4
devgenix <emmanueln644@gmail.com>
2022-02-10 21:39:23 +0100

    Update: modified models to accomodate links greater than 200 in length to 500

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class UrlModel(models.Model):
-    original_url = models.URLField()
+    original_url = models.URLField(max_length=500)
     short_url = models.CharField(max_length=32, unique=True)  # 10 not 6 because there might be a num added to it
 
     def __str__(self):

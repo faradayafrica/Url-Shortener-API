e147d25e396c953daff3b67b17c2237bfdcdcf14
Devgenix <emmaueln644@gmail.com>
2023-02-02 08:33:14 +0100

    update/added path for redirect

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@
 
 from django.urls import path
 
-from core.views import home, Redirect, Shorten
+from core.views import home, Redirect, Shorten, AjaxRedirect
 
 app_name = 'core'
 
 urlpatterns = [
     path('', home),
     path('api/shorten/', Shorten, name='shorten'),
+    path('redirect/pause/', AjaxRedirect, name='pause-redirect'),
     path('<str:url>', Redirect, name='redirect')
 ]
